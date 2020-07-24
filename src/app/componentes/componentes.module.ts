d973025782befb4cc58aa9be7056e1e8c6aa6b7b
Your Name <you@example.com>
2020-07-24 14:44:41 -0300

    commit 24/07(sex)

---
+++ src/app/componentes/componentes.module.ts
@@ -0,0 +1,20 @@
+import { IonicModule } from '@ionic/angular';
+import { MenuComponent } from './menu/menu.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    MenuComponent
+  ],
+  exports: [
+    MenuComponent
+  ],
+  imports: [
+    CommonModule,
+    IonicModule
+  ]
+})
+export class ComponentesModule { }
